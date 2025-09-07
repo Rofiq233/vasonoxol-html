da07eba9dfbe72697f2b9b396f63e03389b2f894
Rofiq233 <rofiqulislamrobiul23@gmail.com>
2025-09-07 09:48:36 +0530

    faq and health section desktop version added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,33 @@ $('.google-review__items').slick({
   speed: 300,
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows:false
-  
+  arrows: false
+
 });
 
 
 $('.google-review__arrow-left').click(function () {
-    $(this).parent().parent().find('.slick-slider').slick('slickPrev');
+  $(this).parent().parent().find('.slick-slider').slick('slickPrev');
 });
 $('.google-review__arrow-right').click(function () {
-    $(this).parent().parent().find('.slick-slider').slick('slickNext');
+  $(this).parent().parent().find('.slick-slider').slick('slickNext');
+});
+
+
+$(document).ready(function () {
+  // Set first item active by default
+  $(".faq__title").first().addClass("active");
+  $(".faq__item-content").first().show();
+
+  $(".faq__title").click(function () {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).parent().find(".faq__item-content").slideUp();
+    } else {
+      $(".faq__title").removeClass("active");
+      $(".faq__item-content").slideUp();
+      $(this).addClass("active");
+      $(this).parent().find(".faq__item-content").slideDown();
+    }
+  });
 });
